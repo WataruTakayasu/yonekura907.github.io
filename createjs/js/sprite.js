79c526673623e775a4607cb8d3c56c93ed37501f
AkioYonekura <yonekura907@gmail.com>
2016-02-07 16:24:23 +0900

    aa

--- createjs/js/sprite.js
+++ createjs/js/sprite.js
@@ -29,7 +29,17 @@ $(function(){
     vitaminC.y = 480;
     vitaminC.alpha = 0;
     // アニメーション
-    createjs.Tween.get(vitaminC).to({alpha:1},500).wait(1000).to({x:500},4000);
+    createjs.Tween.get(vitaminC).to({alpha:1},500).wait(1000).call(vitaminCWalk).to({x:500},4000).call(vitaminCStand);
+    // ビタミンCが歩く
+    function vitaminCWalk(){
+      vitaminC.gotoAndPlay('walk');
+    }
+    // ビタミンCが止まる
+    function vitaminCStand(){
+      vitaminC.gotoAndPlay('stand');
+    }
+
+
 
 
 
