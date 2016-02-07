3a4a745b78ec1414e0f7c819b75b8aeb9a9c4910
yonekura akio <yonekura907@gmail.com>
2016-02-07 11:41:59 +0900

    up

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -5,5 +5,23 @@ $(function(){
         var lon = pos.coords.longitude;
 
         console.log(lat,lon);
+
+        // 定数
+        const apikey = 'f3cdcde042f3a124d0f5c7474e9b0792';
+        // openweathermapのお天気情報のURL
+        var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+ lat +'&lon=' + lon +'&appid=' + apikey;
+
+
+        $.ajax({
+            url:url,
+            dataType:'json'
+        }).done(function(data){
+
+            console.log(data);
+
+        }).fail(function(){
+            console.log('ajax error!!!');
+        });
+
     });
 });
