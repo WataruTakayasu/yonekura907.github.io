df7ddcae20582e08c6f065f2352a27b8f226da99
AkioYonekura <yonekura907@gmail.com>
2016-06-22 16:29:49 +0900

    creates

--- javascript/20160622/createjs/js/ticker.js
+++ javascript/20160622/createjs/js/ticker.js
@@ -1,26 +1,35 @@
-$(function(){
 
-	// stage
-	var stage = new createjs.Stage("canvasEl");
+// stage
+var stage = new createjs.Stage("canvasEl");
+var stageWidth = $("#canvasEl").width();
+var stageHeight = $("#canvasEl").height();
 
-	var circle = new createjs.Shape();
-	circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-	circle.y = 50;
-	stage.addChild(circle);
+var circle = new createjs.Shape();
+circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+circle.y = 50;
+stage.addChild(circle);
 
-	createjs.Ticker.on("tick",tick);
-	createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
-	createjs.Ticker.setFPS(30);
 
-	function tick(ev){
-		// console.log(ev);
+// 星の配置
+var star = new createjs.Shape();
+star.graphics.beginFill("cyan");
+star.graphics.drawPolyStar(0, 0, 40, 5, 0.6, -90);
+star.x = stageWidth/2;
+star.y = stageHeight/2;
+stage.addChild(star);
 
-		circle.x = circle.x + 5;
 
-		if (circle.x > stage.canvas.width){
-			circle.x = 0;
-		};
+createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
+createjs.Ticker.setFPS(30);
+createjs.Ticker.addEventListener("tick",function(evt){
 
-		stage.update();
-	}
+	circle.x = circle.x + 5;
+
+	if (circle.x > stage.canvas.width + 80 ){
+		circle.x = 0;
+	};
+
+	star.rotation += 5*evt.delta*30/1000;
+
+	stage.update();
 });
