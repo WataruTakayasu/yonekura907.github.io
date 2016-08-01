646804d5639d28fa88dad4b59bd2fdf51f4ff2e8
AkioYonekura <yonekura907@gmail.com>
2016-08-01 23:14:41 +0900

    move teu_js

--- javascript/motto_js/6/20151205/main.js
+++
@@ -1,8 +0,0 @@
-;(function(){
-    testModule.show('takeda');
-    testModule.age = 35;
-    testModule.showAge();
-    testModule.name = 'S.Furuya';
-    testModule.showAge();
-    // testModule.show2();
-})();
